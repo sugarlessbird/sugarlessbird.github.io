243b90f136c5814d1f2c722afa9103c929a43527
sugarlessbird <38589969+sugarlessbird@users.noreply.github.com>
2018-06-20 22:24:25 -0600

    javascript fix

--- assignments/lesson-9/js/windchill.js
+++ assignments/lesson-9/js/windchill.js
@@ -10,23 +10,13 @@ var chill = windChill(avg, speed);
 return chill;
 }
 
-/*
-function (temp1, temp2) {
-     tempF = temp1 - temp2 / 2;
- }
-*/
 
 function windChill(tempF, speed) {   
 var windChillvalue = 35.74 + (0.6215 * tempF) - 35.75 * Math.pow(speed,.16) + 0.4275 * tempF * Math.pow(speed,.16);
 var multiplier = Math.pow(10, 2);
 windChillvalue = Math.round(windChillvalue * multiplier) / multiplier;
-	/*if(windChillvalue>=70){
-	windChillvalue = "Ewwww! It is <strong><font color=red>" + windChillvalue + " F</font></strong> Degrees with the Wind Chill!";
-	}
-	else{
-	windChillvalue = "Brrrr! It is <strong><font color=blue>" + windChillvalue + " F</font></strong> Degrees with the Wind Chill!";
-	}*/
 var clean = parseInt(windChillvalue);    
 return clean;
 }
    
+ 
