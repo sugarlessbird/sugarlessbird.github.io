e158b301196ab17040ec5773a56f02c490a74e36
sugarlessbird <38589969+sugarlessbird@users.noreply.github.com>
2018-06-20 18:30:18 -0600

    lesson 9 index page

---
+++ assignments/lesson-8/js/weatherapi.js
@@ -0,0 +1,23 @@
+
+
+
+
+var weatherRequest= new XMLHttpRequest();
+    weatherRequest.open('GET','//api.openweathermap.org/data/2.5/weather?id=4156210&appid=9967347d49e1d70dbd0c0f7eb60694d2&units=imperial',true);
+    
+    weatherRequest.send();
+
+    weatherRequest.onload = function() {
+var weatherData = JSON.parse(weatherRequest.responseText);
+        console.log(weatherData);
+        
+        document.getElementById('current-temp').innerHTML=weatherData.main.temp;
+        document.getElementById('weather-description').innerHTML=weatherData.weather[0].main;
+        document.getElementById('high-temp').innerHTML=weatherData.main.temp_max;
+        document.getElementById('low-temp').innerHTML=weatherData.main.temp_min;
+        document.getElementById('wind-speed').innerHTML=weatherData.wind.speed;
+        var iconcode=weatherData.weather[0].icon;
+        var icon_path="//openweathermap.org/img/w/"+ iconcode +".png";
+        document.getElementById('weather_icon').src = icon_path;
+}
+
---
+++ assignments/lesson-9/js/windchill.js
@@ -0,0 +1,32 @@
+document.getElementById("windchill").innerHTML=doInputOutput();
+
+
+function doInputOutput() {
+var high = parseFloat(document.getElementById('high-temp').innerHTML);
+var low = parseFloat(document.getElementById('low-temp').innerHTML);
+var speed = parseFloat(document.getElementById('wind-speed').innerHTML);    
+var avg = (high+low)/2;
+var chill = windChill(avg, speed);
+return chill;
+}
+
+/*
+function (temp1, temp2) {
+     tempF = temp1 - temp2 / 2;
+ }
+*/
+
+function windChill(tempF, speed) {   
+var windChillvalue = 35.74 + (0.6215 * tempF) - 35.75 * Math.pow(speed,.16) + 0.4275 * tempF * Math.pow(speed,.16);
+var multiplier = Math.pow(10, 2);
+windChillvalue = Math.round(windChillvalue * multiplier) / multiplier;
+	/*if(windChillvalue>=70){
+	windChillvalue = "Ewwww! It is <strong><font color=red>" + windChillvalue + " F</font></strong> Degrees with the Wind Chill!";
+	}
+	else{
+	windChillvalue = "Brrrr! It is <strong><font color=blue>" + windChillvalue + " F</font></strong> Degrees with the Wind Chill!";
+	}*/
+var clean = parseInt(windChillvalue);    
+return clean;
+}
+   
